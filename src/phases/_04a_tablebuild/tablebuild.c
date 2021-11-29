b6267ff93a68106b34738067fe257cc96667ece6
Paul Hoffmann <paul.hoffmann@iem.thm.de>
2021-11-29 16:46:06 +0100

    WIP

--- src/phases/_04a_tablebuild/tablebuild.c
+++ src/phases/_04a_tablebuild/tablebuild.c
@@ -25,9 +25,75 @@ static void printSymbolTableAtEndOfProcedure(Identifier *name, Entry *procedureE
     printf("\n");
 }
 
+Type* createTypeForTypeExpression(TypeExpression* e, SymbolTable* table, Position pos) {
+    switch (e->kind) {
+        case TYPEEXPRESSION_NAMEDTYPEEXPRESSION:
+            ;
+            Identifier *identifier = e->u.namedTypeExpression.name;
+
+            // gibt es den Typ überhaupt?
+            if (lookup(table, identifier) == NULL) {
+                notAType(pos, identifier);
+            }
+
+            return newPrimitiveType(identifier->string, INT_BYTE_SIZE);//TODO: check if this is correct
+            break;
+        case TYPEEXPRESSION_ARRAYTYPEEXPRESSION:
+            return newArrayType(e->u.arrayTypeExpression.arraySize, createTypeForTypeExpression(e->u.arrayTypeExpression.baseType));
+            break;
+    }
+}
+
+ParameterTypeList* createParameterTypeList(ParameterDeclarationList* pdl, SymbolTable* table, Position pos) {
+    ParameterTypeList * ptl = emptyParameterTypeList();
+    while(!pdl->isEmpty){
+        //new parameter type
+        ParameterDeclaration *current = pdl->head;
+        pdl = pdl->tail;
+        Type *type = createTypeForTypeExpression(current->typeExpression, table, pos);
+        ptl = newParameterTypeList(type, ptl);
+    }
+}
+
 SymbolTable *buildSymbolTable(Program *program, bool showSymbolTables) {
     intType = newPrimitiveType("int", INT_BYTE_SIZE);
     boolType = newPrimitiveType("boolean", BOOL_BYTE_SIZE);
+
+	GlobalDeclarationList *decList = program->declarations;
+    SymbolTable *globalTable = initializeGlobalTable();
+
+    while(!decList->isEmpty){
+        GlobalDeclaration *current = decList->head;
+        decList = decList->tail;
+        switch (current->kind) {
+            case DECLARATION_TYPEDECLARATION:
+                //new Entry for the type declaration
+                ;
+                Type *type = createTypeForTypeExpression(current->u.typeDeclaration.typeExpression, globalTable, current->position);
+                Entry *type_entry = enter(globalTable, current->name, newTypeEntry(type));
+                if (type_entry == NULL) {
+                    redeclarationAsType(current->position, current->name);
+                }
+                break;
+            case DECLARATION_PROCEDUREDECLARATION:
+                //new Entry for the procedure declaration
+                ;
+                ParameterTypeList *parameterTypeList = createParameterTypeList(current->u.procedureDeclaration.parameters, globalTable, current->position);
+                Entry *proc_entry = enter(globalTable, current->name, newProcEntry(parameterTypeList, newTable(globalTable)));
+                if(proc_entry == NULL) {
+                    redeclarationAsProcedure(current->position, current->name);
+                }
+                break;
+
+        }
+        // type x = intType;
+        // type x = intType
+
+
+
+
+
+    }
 
     //TODO (assignment 4a): Initialize a symbol table with all predefined symbols and fill it with user-defined symbols
     notImplemented();
