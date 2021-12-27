d2d61618830b6be9746d8733917cb7001bb42458
Paul Hoffmann <paul.hoffmann@iem.thm.de>
2021-12-27 12:58:11 +0100

    WIP anfang von getTypeofExpression

--- src/phases/_04b_semant/procedurebodycheck.c
+++ src/phases/_04b_semant/procedurebodycheck.c
@@ -47,9 +47,11 @@ Type *getTypeOfVariable(
 
     switch (variable->kind) {
         case VARIABLE_ARRAYACCESS:
+            //rekursiv den typ der variable ermitteln
             return getTypeOfVariable(variable->u.arrayAccess.array, localTable);
         case VARIABLE_NAMEDVARIABLE:
             ;
+            //variable muss in symvoltable sein und eine variable sein
             Identifier *name = variable->u.namedVariable.name;
             Entry* lookedUp = lookup(localTable, name);
             if(lookedUp == NULL){
@@ -62,6 +64,67 @@ Type *getTypeOfVariable(
             return t;
     }
 }
+
+Type *getTypeOfExpression(
+        Expression *exp,
+        SymbolTable *local_table
+        ) {
+    switch (exp->kind) {
+        // 5
+        case EXPRESSION_INTLITERAL:
+            return intType;
+
+            // a
+        case EXPRESSION_VARIABLEEXPRESSION:
+            ;
+            Variable *variable = exp->u.variableExpression.variable;
+
+            variable->dataType = getTypeOfVariable(variable, local_table);
+            //TODO error checks
+            //vermutlich diese
+//            void indexingNonArray(Position position);
+//            void indexingWithNonInteger(Position position);
+
+            return exp->u.variableExpression.variable->dataType;
+
+            //3 + 5
+        case EXPRESSION_BINARYEXPRESSION:
+            ;
+            Expression *left = exp->u.binaryExpression.leftOperand;
+            Expression *right = exp->u.binaryExpression.rightOperand;
+            BinaryOperator operator = exp->u.binaryExpression.operator;
+
+            left->dataType = getTypeOfExpression(left, local_table);
+            right->dataType = getTypeOfExpression(right, local_table);
+            if (left->dataType != right->dataType) {
+                operatorDifferentTypes(exp->position);
+            }
+
+            if (left->dataType->kind != TYPE_KIND_PRIMITIVE ||
+                right->dataType->kind != TYPE_KIND_PRIMITIVE) {
+                switch (operator) {
+                    case ABSYN_OP_ADD:
+                    case ABSYN_OP_SUB:
+                    case ABSYN_OP_MUL:
+                    case ABSYN_OP_DIV:
+                        arithmeticOperatorNonInteger(exp->position);
+                        break;
+                    case ABSYN_OP_EQU:
+                    case ABSYN_OP_NEQ:
+                    case ABSYN_OP_GRE:
+                    case ABSYN_OP_GRT:
+                    case ABSYN_OP_LSE:
+                    case ABSYN_OP_LST:
+                        comparisonNonInteger(exp->position);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            break;
+    }
+}
+
 void checkStatement(
         Statement* statement,
         SymbolTable* localTable
@@ -70,12 +133,26 @@ void checkStatement(
         case STATEMENT_ASSIGNSTATEMENT:
             // TODO dataType is noch NULL
             ;
-            Type* t = getTypeOfVariable(statement->u.assignStatement.target, localTable);
+
+            Variable *target = statement->u.assignStatement.target;
+            Expression *value = statement->u.assignStatement.value;
+
+            //variable muss in symvoltable sein und eine variable sein
+            //rekursiv den typ der variable ermitteln
+            target->dataType = getTypeOfVariable(target, localTable);
+
+//            statement->u.assignStatement.value->dataType = getTypeOfExpression(statement->u.assignStatement.value, localTable);
+            value->dataType = getTypeOfExpression(value, localTable);
 
 
-//            if (statement->u.assignStatement.target->dataType !=statement->u.assignStatement.value->dataType){
-//                assignmentHasDifferentTypes(statement->position);
-//            }
+            //prüfen dass links und rechts typkompatible sind
+            if (target->dataType != value->dataType){
+                assignmentHasDifferentTypes(statement->position);
+            }
+            if (value->dataType->kind != TYPE_KIND_PRIMITIVE){
+                notImplemented();
+                assignmentRequiresIntegers(statement->position);
+            }
 
             break;
         case STATEMENT_CALLSTATEMENT:
