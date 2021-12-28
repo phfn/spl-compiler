ec3ab8c34e379aa1e9c7bbc170fa82d140c60c75
Paul Hoffmann <paul.hoffmann@iem.thm.de>
2021-12-28 12:51:24 +0100

    Add semantic tests script

---
+++ test-semant.py
@@ -0,0 +1,30 @@
+#!/bin/python
+import os
+import filecmp
+from subprocess import call, run
+def get_files(folder: str) -> list:
+    files = []
+    for el in os.listdir(folder):
+        path = os.path.join(folder, el)
+        if os.path.isfile(path):
+            files.append(path)
+        else:
+            files = files + get_files(path)
+    return files
+
+
+files = [file for file in get_files("progs/semant") if ".spl" in str(file)]
+files.sort()
+spl_ref = "./eco32tools/bin/refspl"
+spl_our = "./build/spl"
+
+for file in files:
+    our = run([spl_our, "--semant", file], capture_output=True, check=False)
+    ref = run([spl_ref, "--semant", file], capture_output=True, check=False)
+    if our.stdout == ref.stdout:
+        print(f"{file} ✓")
+    else:
+        print(f"{file} ✕")
+        print(f"OUR: {our.stdout.decode()}{our.stderr.decode()}")
+        print(f"REF: {ref.stdout.decode()}{ref.stderr.decode()}")
+
