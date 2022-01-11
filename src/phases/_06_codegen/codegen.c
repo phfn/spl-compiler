7aac324a84200887ca9ae689021071c2474ce6a2
Paul Hoffmann <paul.hoffmann@iem.thm.de>
2022-01-11 11:05:03 +0100

    codegen: Comparison BinaryOpererator
    Ich hab den BinaryOperator in Vergleichs- und Arithmetikoperatoren
    geteilt. Grund war dass wir keinen Befehl haben der (wie zB in ninja)
    Springt wenn auf dem Stack ein True liegt. Wir können nur springen,
    wenn ein Vergleich erfüllt wird. Die WhileSchleife will Springen wenn
    der Vergleich fehlschlägt. Deshalb wird gesprungen wenn der jeweils
    gegenteilige operator stimmt.
    Beispiel:
    while(i<5){
        i := i+1;
    }
    würde zu:
    lege wert von i auf den stack
    lege 5 auf den stack
    springe wenn rsp-1(i) >= rsp-1(5) hinter das while statement
    führe das while statement aus
    
    Ausserdem habe ich einen define label "COPATIBLE_MODE" hinzugefügt.
    Ich find die Labelbezeichnungen in der referenz nicht so gut,
    will aber auch den gleichen output zum diffen.
    Deshalb gibt es nun diesen Modus.

--- src/phases/_06_codegen/codegen.c
+++ src/phases/_06_codegen/codegen.c
@@ -24,6 +24,7 @@
 #define FRAME_POINTER  25
 #define STACK_POINTER  29
 
+// #define COPATIBLE_MODE
 int register_stack_pointer = FIRST_REGISTER - 1;
 int label_counter = 0;
 
@@ -74,7 +75,32 @@ void assemblerProlog(FILE *out) {
 
 
 
-void genBinaryOperator(BinaryOperator operator, FILE *out){
+void genReversedComparisonBinaryOperator(BinaryOperator operator, FILE *out, int op1, int op2, char* label){
+	switch(operator){
+		case ABSYN_OP_EQU:
+			emitRRL(out, "bne", op1, op2, label);
+			break;
+		case ABSYN_OP_NEQ:
+			emitRRL(out, "beq", op1, op2, label);
+			break;
+		case ABSYN_OP_GRT:
+			emitRRL(out, "ble", op1, op2, label);
+			break;
+		case ABSYN_OP_GRE:
+			emitRRL(out, "blt", op1, op2, label);
+			break;
+		case ABSYN_OP_LST:
+			emitRRL(out, "bge", op1, op2, label);
+			break;
+		case ABSYN_OP_LSE:
+			emitRRL(out, "bgt", op1, op2, label);
+			break;
+		default:
+			error("Expected comparison operator but found arithmetic");
+		}
+
+}
+void genArithmeticBinaryOperator(BinaryOperator operator, FILE *out){
 	switch(operator){
 		case ABSYN_OP_ADD:;
 			emitRRI(out, "add", register_stack_pointer-1, register_stack_pointer-1, register_stack_pointer);
@@ -88,24 +114,8 @@ void genBinaryOperator(BinaryOperator operator, FILE *out){
 		case ABSYN_OP_DIV:
 			emitRRI(out, "div", register_stack_pointer-1, register_stack_pointer-1, register_stack_pointer);
 			break;
-		case ABSYN_OP_EQU:
-			notImplemented();
-			break;
-		case ABSYN_OP_NEQ:
-			notImplemented();
-			break;
-		case ABSYN_OP_GRT:
-			notImplemented();
-			break;
-		case ABSYN_OP_GRE:
-			notImplemented();
-			break;
-		case ABSYN_OP_LST:
-			notImplemented();
-			break;
-		case ABSYN_OP_LSE:
-			notImplemented();
-			break;
+		default:
+			error("Expected arithetic operator but found comparision");
 	}
 	decrease_stack_pointer();
 }
@@ -124,14 +134,27 @@ void genVariableExpression(Expression *expression, SymbolTable *local_table, FIL
 	genVariable(variable, local_table, out);
 }
 
-void genBinaryExpression(Expression *expression, SymbolTable *local_table, FILE *out){
+void genArithmeticBinaryExpression(Expression *expression, SymbolTable *local_table, FILE *out){
 	Expression *leftOperand = expression->u.binaryExpression.leftOperand;
 	Expression *rightOperand = expression->u.binaryExpression.rightOperand;
 	BinaryOperator operator = expression->u.binaryExpression.operator;
 
 	genExpression(leftOperand, local_table, out);
 	genExpression(rightOperand, local_table, out);
-	genBinaryOperator(operator, out);
+	genArithmeticBinaryOperator(operator, out);
+}
+void genJumpIfComparisonIsFalse(Expression *comparisonExpression, SymbolTable *local_table, FILE *out, char* label){
+	Expression *leftOperand = comparisonExpression->u.binaryExpression.leftOperand;
+	Expression *rightOperand = comparisonExpression->u.binaryExpression.rightOperand;
+	BinaryOperator operator = comparisonExpression->u.binaryExpression.operator;
+
+	genExpression(leftOperand, local_table, out);
+	int index_left = register_stack_pointer;
+	genExpression(rightOperand, local_table, out);
+	int index_right = register_stack_pointer;
+	genReversedComparisonBinaryOperator(operator, out, index_left, index_right, label);
+	decrease_stack_pointer();
+	decrease_stack_pointer();
 }
 
 void genExpression(Expression *expression, SymbolTable *local_table, FILE *out){
@@ -143,7 +166,7 @@ void genExpression(Expression *expression, SymbolTable *local_table, FILE *out){
 			genVariableExpression(expression, local_table, out);
 			break;
 		case EXPRESSION_BINARYEXPRESSION:
-			genBinaryExpression(expression, local_table, out);
+			genArithmeticBinaryExpression(expression, local_table, out);
 			break;
 	}
 }
@@ -199,6 +222,8 @@ void genAssignStatement(Statement *statement, SymbolTable *local_table, FILE *ou
 
 	//assign					src						reg				offset
 	emitRRI(out, "stw", register_stack_pointer, register_stack_pointer-1, 0);
+	decrease_stack_pointer();
+	decrease_stack_pointer();
 	
 }
 void genIfStatement(Statement *statement, SymbolTable *local_table, FILE *out){
@@ -208,19 +233,23 @@ void genWhileStatement(Statement *statement, SymbolTable *local_table, FILE *out
 	Expression *condition = statement->u.whileStatement.condition;
 	Statement *body = statement->u.whileStatement.body;
 
-	int label_index = ++label_counter;
+	int label_index = label_counter++;
 	char end_label[100] ;
 	char start_label[100] ;
-	sprintf(start_label, "while_start_%08d", label_index);
-	sprintf(end_label, "while_end_%08d", label_index);
+
+#ifdef COPATIBLE_MODE
+	sprintf(start_label, "L%d", label_index);
+	label_index = label_counter++;
+	sprintf(end_label, "L%d", label_index);
+#else
+	sprintf(start_label, "while_start_%d", label_index);
+	sprintf(end_label, "while_end_%d", label_index);
+#endif
 
 	emitLabel(out, start_label);
-	genExpression(condition, local_table, out);
-	int index_condition = register_stack_pointer;
+	genJumpIfComparisonIsFalse(condition, local_table, out, end_label);
 
 	//if ergebniss der expression != true
-	int index_true = pushc(true, out);
-	emitRRL(out, "bne", index_condition, index_true, end_label);
 	genStatement(body, local_table, out);
 	emitJump(out, start_label);
 
